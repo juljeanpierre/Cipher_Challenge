025a06dc305a84ab4c3976e350db72a568a69982
Jean-Pierre J <juljeanpierre@gmail.com>
2020-05-30 11:02:24 +0200

    Initial commit
    source files

---
+++ script.js
@@ -0,0 +1,13 @@
+function myFunction() {
+  var pass = window.prompt("ZL URNEG VF UHZNA, ZL OYBBQ VF OBVYVAT, ZL OENVA VOZ. JUB NZ V?","");
+  var answer = "Mr.Roboto";
+
+  if (pass == answer) {
+    alert("Domo arigato 😉");
+    window.location.assign("https://www.whoismrrobot.com/");
+  }
+  else {
+    alert("🙅‍ Try again ");
+    window.location.reload();
+  }
+}
